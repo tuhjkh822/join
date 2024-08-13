86acf06a00140c7be6ecf536b6d9887d094095ca
tuhjkh822 <wcujdhkw@outlook.com>
2024-08-13 16:20:13 +0800

    Create join.java

---
+++ join.java
@@ -0,0 +1,32 @@
+num_int = 123
+num_str = "456"
+
+print("num_int 数据类型为:",type(num_int))
+print("num_str 数据类型为:",type(num_str))
+
+print(num_int+num_str)
+num_int = 123
+num_str = "456"
+
+print("num_int 数据类型为:",type(num_int))
+print("类型转换前，num_str 数据类型为:",type(num_str))
+
+num_str = int(num_str)    # 强制转换为整型
+print("类型转换后，num_str 数据类型为:",type(num_str))
+
+num_sum = num_int + num_str
+
+print("num_int 与 num_str 相加结果为:",num_sum)
+print("sum 数据类型为:",type(num_sum))
+num_int 数据类型为: <class 'int'>
+类型转换前，num_str 数据类型为: <class 'str'>
+类型转换后，num_str 数据类型为: <class 'int'>
+num_int 与 num_str 相加结果为: 579
+sum 数据类型为: <class 'int'>
+#!/usr/bin/python3
+ 
+var1 = 'Hello World!'
+var2 = "Runoob"
+ 
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[1:5])
